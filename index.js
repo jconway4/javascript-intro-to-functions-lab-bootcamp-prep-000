e7f338cd891be5dcde21be034f1d9fba93ea9b97
jconway4 <jconwa4@illinois.edu>
2018-03-13 23:54:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
 function sayHiToGrandma(string){
   if (string === 'I love you, Grandma.') {
     return "I love you, too."
